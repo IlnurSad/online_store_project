46352d0ec209e4b7cbe7406f1b75b8cc1304b2c3
IlnurSad <sadykovilnurr@gmail.com>
2023-01-06 01:49:52 +0300

    change2

--- src/com/ilnur/jdbc/DaoRunner.java
+++ src/com/ilnur/jdbc/DaoRunner.java
@@ -8,6 +8,17 @@ import java.time.LocalDate;
 public class DaoRunner {
 
     public static void main(String[] args) {
+        saveTest();
+        deleteTest();
+    }
+
+    private static void deleteTest() {
+        var customerDao = CustomerDao.getInstance();
+        var deleteResult = customerDao.delete(1);
+        System.out.println(deleteResult);
+    }
+
+    private static void saveTest() {
         var customerDao = CustomerDao.getInstance();
         var customer = new Customer();
         customer.setFirstName("Lenar");
